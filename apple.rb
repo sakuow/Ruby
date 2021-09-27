a948fbd370d2b93bd4261a1bafce4b0da51106b2
sakuow <yusaku1335@gmail.com>
2021-09-27 02:20:52 +0000

    4 commit

---
+++ apple.rb
@@ -0,0 +1,18 @@
+apple = "Nagano"
+
+if apple == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません。"
+end
+
+apple2 = "Yamanashi"
+if apple2 == "Aomori"
+  puts "このリンゴは青森県産です。"
+elsif apple2 == "Nagano"
+  puts "このリンゴは青森県産ではなく、長野県産です。"
+else
+  puts "このリンゴは青森県産でも長野県産でもありません。"
+end
